adb6dc6e6c66a2e17d50c0d77f7a6b7457251e89
SqrtMz <cvesgaduran@gmail.com>
2024-06-26 19:30:52 -0500

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,21 +1,18 @@
 #! /bin/bash
 
-if [ $DESKTOP_SESSION == "hyprland" ]
-then
+if [[ $DESKTOP_SESSION == "hyprland" ]]; then
 
-    hyprpaper
+    hyprpaper &
 
-elif [ $DESKTOP_SESSION == "sway" ]
-then
+elif [[ $DESKTOP_SESSION == "sway" ]]; then
 
-    swaybg -i ~/Pictures/"Images & Videos"/ZZ.png
-    #--no-startup-id /usr/lib/xdg-desktop-portal -r
-    
+    swaybg -i ~/Pictures/"Images & Videos"/ZZ.png &
+
 fi
 
-waybar
-nm-applet
-wl-paste --type text --watch cliphist store
-wl-paste --type image --watch cliphist store
-systemctl --user enable opentabletdriver --now
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+waybar &
+nm-applet &
+wl-paste --type text --watch cliphist store &
+wl-paste --type image --watch cliphist store &
+systemctl --user enable opentabletdriver --now &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
