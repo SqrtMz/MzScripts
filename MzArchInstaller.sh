b68b07f0ec55e0e602fcf8646eb2772467cff0da
SqrtMz <cvesgaduran@gmail.com>
2024-06-27 20:21:23 -0500

    not yet, tristemente

--- MzArchInstaller.sh
+++ MzArchInstaller.sh
@@ -168,24 +168,7 @@ arch-chroot /mnt /bin/bash -e <<EOF
     su $user -c "xdg-user-dirs-update"
 
     # Grub config
-    while true
-    do
-        echo "Is this an removable device installation? [y/n]"
-        read tGb
-
-        case $tGb in
-            [Yy]* ) echo "Installing removable media Grub"
-                    grub-install --target=x86_64-efi --efi-directory=/boot --removable
-                    break;;
-            
-            [Nn]* ) echo "Installing non-removable media Grub"
-                    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=$hostname
-                    break;;
-            
-            * ) echo "Invalid option, try again"
-                continue;;
-        esac
-    done
+    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=$hostname
 
     echo "" >> /etc/default/grub
     echo "# Last selected OS" >> /etc/default/grub
